9945d089040f7f7aac41c30f00352a1a5a57f488
someilay <ismilioshyn@gmail.com>
2021-04-02 15:47:06 +0300

    graph-0.01

---
+++ Graph.h
@@ -0,0 +1,18 @@
+#ifndef GRAPH_GRAPH_H
+#define GRAPH_GRAPH_H
+#include "LinkedList.h"
+
+#define DIRECTED 1
+#define SELF_LOOP 0
+
+struct graph{
+    int (*numberOfNodes)(void* this);
+    void (*add)(void* this, void* object);
+    void (*addEdge)(void* this, int i, int j);
+    void* (*remove)(void* this, int i);
+    void (*removeEdge)(void* this, int i, int j);
+} typedef Graph;
+
+Graph* createGraph();
+
+#endif
