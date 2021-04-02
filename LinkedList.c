a69815ca7eb6b9b3520c5dc2ec372c2ecc0e8d77
someilay <ismilioshyn@gmail.com>
2021-04-02 14:17:48 +0300

    linked_list-0.03

--- LinkedList.c
+++ LinkedList.c
@@ -23,9 +23,105 @@ static Container* getLastContainer(void* this){
     return out;
 }
 
-int size(void* this){
+static int* p_size(void* this){
     Private* aPrivate = getPrivate(this);
-    return *((int*)(aPrivate->privateField[SIZE]));
+    return (int*)(aPrivate->privateField[SIZE]);
+}
+
+int size(void* this){
+    return *p_size(this);
+}
+
+int isEmpty(void* this){
+    return size(this) == 0;
+}
+
+void addLast(void* this, void* object){
+    Container* last = getLastContainer(this);
+    int* size = p_size(this);
+
+    Container* newContainer = createContainer(object, last, last->previous);
+    last->previous->next = newContainer;
+    last->previous = newContainer;
+    (*size)++;
+}
+
+void add(void* this, void* object, int i){
+    Container* first = getFirstContainer(this);
+    Container* last = getLastContainer(this);
+    int* size = p_size(this);
+
+    Container* cur = first->next;
+    int j = 0;
+    while (cur != last && j != i){
+        cur = cur->next;
+    }
+
+    if (cur != last){
+        Container* newContainer = createContainer(object, cur, cur->previous);
+        cur->previous->next = newContainer;
+        cur->previous = newContainer;
+        (*size)++;
+    }
+}
+
+void* removeLast(void* this){
+    Container* first = getFirstContainer(this);
+    Container* last = getLastContainer(this);
+    int* size = p_size(this);
+
+    Container* lastContainer = last->previous;
+
+    if (lastContainer != first){
+        lastContainer->previous->next = lastContainer->next;
+        lastContainer->next->previous = lastContainer->previous;
+        (*size)--;
+
+        void *lastObject = lastContainer->object;
+        free(lastContainer);
+
+        return lastObject;
+    }
+
+    return NULL;
+}
+
+void* remove(void* this, int i){
+    Container* first = getFirstContainer(this);
+    Container* last = getLastContainer(this);
+    int* size = p_size(this);
+
+    Container* cur = first->next;
+    int j = 0;
+    while (cur != last && j != i){
+        cur = cur->next;
+    }
+
+    if (cur != last){
+        cur->previous->next = cur->next;
+        cur->next->previous = cur->previous;
+        (*size)--;
+
+        void *lastObject = cur->object;
+        free(cur);
+
+        return lastObject;
+    }
+
+    return NULL;
+}
+
+void* get(void* this, int i){
+    Container* first = getFirstContainer(this);
+    Container* last = getLastContainer(this);
+
+    Container* cur = first->next;
+    int j = 0;
+    while (cur != last && j != i){
+        cur = cur->next;
+    }
+
+    return cur->object;
 }
 
 LinkedList *createLinkedList() {
@@ -48,6 +144,12 @@ LinkedList *createLinkedList() {
     p_out[sizeof(LinkedList) / 4] = (void*)aPrivate;
 
     out->size = size;
+    out->isEmpty = isEmpty;
+    out->addLast = addLast;
+    out->add = add;
+    out->removeLast = removeLast;
+    out->remove = remove;
+    out->get = get;
 
     return out;
 }
