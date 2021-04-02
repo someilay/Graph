5c623ada137d9010512b1a86991bc30b180439b6
someilay <ismilioshyn@gmail.com>
2021-04-02 16:59:08 +0300

    graph-0.02

--- Graph.c
+++ Graph.c
@@ -1,1 +1,19 @@
+#include <malloc.h>
 #include "Graph.h"
+#include "Private.h"
+#include "LinkedList.h"
+
+#define NUMBER_OF_PRIVATE_FIELDS 1
+#define LIST_OF_NODES 0
+
+Graph *createGraph() {
+    Graph* out = malloc(sizeof(Graph) + sizeof(void*));
+    void** p_out = (void**)out;
+    Private* aPrivate = createPrivate(NUMBER_OF_PRIVATE_FIELDS);
+    LinkedList* listOfNodes = createLinkedList();
+
+    aPrivate->privateField[LIST_OF_NODES] = listOfNodes;
+    p_out[sizeof(Graph) / 4] = aPrivate;
+
+    return out;
+}
